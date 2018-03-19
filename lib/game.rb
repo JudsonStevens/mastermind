a9f4943f3b3410c31df276f88d7bbdc1ff28689b
Judson Stevens <Judson.Stevens2gmail.com>
2018-03-19 07:42:30 -0600

    Final tweaks, need to put in more comments.

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,7 @@ class Game
               :starting_time,
               :printer,
               :length_of_answer
-
+#Initialize all of the variables needed to operate the game, including the answer, history, printer, and top ten list.
   def initialize(length_of_answer, number_of_colors)
     @answer_code = generate_answer(length_of_answer, number_of_colors)
     @amount_of_colors = number_of_colors
@@ -24,10 +24,13 @@ class Game
     @top_ten_list = TopTenList.new
   end
 
+#Get user input, elminate the new lines, and eliminate any whitespace.
   def get_game_input
     @user_input = gets.chomp.strip
   end
 
+#Generating the answer code based on the input of difficulty which changes the length of the answer
+#and how many variables it uses to generate the answer.
   def generate_answer(length_of_answer, number_of_colors)
     if number_of_colors == 4
       charset = Array["r", "b", "g", "y"]
@@ -39,6 +42,10 @@ class Game
     Array.new(length_of_answer) { charset.sample }
   end
 
+#Main game loop after the initial start. Until the user inputs the quit criteria, it loops through
+#the different if statements to determine what path to take. Need to divide up some of these responses
+#and put them into methods. Also need to define methods with the criteria for the if statements to
+#make it more succinct. Could the exit be placed somewhere else or gotten rid of entirely?
   def new_game
     until @user_input == "q" || @user_input == "quit"
       get_game_input
@@ -67,16 +74,24 @@ class Game
     end
   end
 
+#Method to receive the user guess, store it in an array, and then check it using
+#the checker class.
   def intake_guess(user_guess, answer_code)
     @history_of_guesses << @user_input
     @results = AnswerChecker.answer_check(user_guess, answer_code)
   end
 
+#Used to display the array of user guesses and then asks the user to continue guessing.
   def display_guess_history
     puts @history_of_guesses
     @printer.continue_guesses
   end
 
+#When the user inputs the correct answer, display a win message with time and the original guess,
+#offer congratulations, then get the user name in order to store it, compare it to the average,
+#and list it in the top ten if valid. After that, asks if the user wants to play again.
+#Need to compress the top ten list methods into one method inside the top ten class
+#and utilize one line of code here.
   def winner
     time_keeper
     @printer.win_message(@user_input, @history_of_guesses.length, @finished_time)
@@ -89,12 +104,15 @@ class Game
     InitialInput.new.play
   end
 
+#Keeps time and stores it into an array once the user wins.
   def time_keeper
     minutes = ((Time.now - @starting_time)/60).to_i
     seconds = ((Time.now - @starting_time) % 60).to_i
     @finished_time = [minutes, seconds]
   end
 
+#Short method to display the answer in the cheat method in the game statements class.
+#Needs to be reworked.
   def display_answer_code(answer)
     answer = @answer_code
   end
--- lib/top_ten_list.rb
+++ lib/top_ten_list.rb
@@ -11,6 +11,7 @@ class TopTenList
     @name = gets.chomp.strip
   end
 
+#Storing the name, time, and number of guesses as a hash into a YAML file in order to be recalled later.
   def store_name_and_time(answer, time, number_of_guesses)
     storage = Hash.new
     storage["name and answer"] = [@name, answer.upcase]
@@ -21,17 +22,20 @@ class TopTenList
     end
   end
 
+#Loading the top ten list, sorting it, then storing it into an array.
   def retrieve_top_ten_values
     @top_ten_info = []
-    YAML.load_stream(File.read 'top_ten.yml') { |x| @top_ten_info << x }
+    YAML.load_stream(File.read 'top_ten.yml') { |entries| @top_ten_info << entries }
     @sorted_top_ten_list = @top_ten_info.sort_by { |v| [ v["number of guesses"], v["time"]]}
   end
 
+#Calculating the average time to solve the game and storing that info in an array.
   def average_time_to_solve
     retrieve_top_ten_values
     @time_total = @top_ten_info.map { |y| y["time"]}.reduce(:+) / @top_ten_info.size
   end
 
+#Uses two other methods in order to display the users time versus the average time.
   def report_time_difference(time)
     new_user_time = (time[0]*60) + time[1]
     if new_user_time > average_time_to_solve
@@ -43,6 +47,7 @@ class TopTenList
     end
   end
 
+#Prints if the users time is under the average time after calculating the difference.
   def under_average_time(new_user_time)
     time_difference = @time_total - new_user_time
     new_time_difference_minutes = time_difference / 60
@@ -50,6 +55,7 @@ class TopTenList
     print "#{new_time_difference_minutes} minute(s), #{new_time_difference_seconds} second(s) faster than"
   end
 
+#Prints if the users time is over the average time after calculating the difference.
   def over_average_time(new_user_time)
     time_difference = new_user_time - @time_total
     new_time_difference_minutes = time_difference / 60
@@ -57,11 +63,13 @@ class TopTenList
     print "#{new_time_difference_minutes} minute(s), #{new_time_difference_seconds} second(s) slower than"
   end
 
-
+#Stores the average number of guesses into an array. Variable needs to be more descriptive in the map statement.
   def average_count_to_solve
     @average_number_of_guesses = @top_ten_info.map { |x| x["number of guesses"]}.reduce(:+) / @top_ten_info.size
   end
 
+#Prints the information compiled about the average guesses and time, and takes in arguments from
+#outside to print out the original answer, time, and numbers of guesses.
   def user_completion_stats(answer, time, number_of_guesses)
     puts "\n"
     print "#{@name}, you guessed the sequence #{answer.upcase} in #{number_of_guesses} guesse(s) over #{time[0]} minute(s), #{time[1]} second(s)."
@@ -70,6 +78,8 @@ class TopTenList
     puts "#{guess_amount_difference(number_of_guesses)} the average."
   end
 
+#Displays the amount of guesses either over or under the average deending on the data.
+#Need to combine actions into methods and utilize.
   def guess_amount_difference(number_of_guesses)
     if number_of_guesses > average_count_to_solve
       difference_in_number_of_guesses = number_of_guesses - average_count_to_solve
@@ -82,6 +92,9 @@ class TopTenList
     end
   end
 
+#Prints out the top ten list. Need to take variable inside the puts line and store them elsewhere
+#in order to shorten the amount of code for that one line. However, the line cannot
+#be broken up in order to be displayed in the correct format.
   def top_ten_ranking
     retrieve_top_ten_values
     print_top_ten_list = @sorted_top_ten_list.first(10).compact
